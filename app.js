a4b285f039d3f23e40d5784713c3489f8b884b0a
Alexander Sirotin <Alexander.Sirotin@gmail.com>
2015-10-20 04:10:00 +0300

    ExtJS app

---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
+ */
+Ext.application({
+    name: 'ExtJS5Grid',
+
+    extend: 'ExtJS5Grid.Application',
+    
+    //autoCreateViewport: 'ExtJS5Grid.view.main.Main'
+    autoCreateViewport: 'ExtJS5Grid.view.credit.Credit'
+	
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to ExtJS5Grid.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
+});
